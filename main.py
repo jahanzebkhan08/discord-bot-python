7a573d7b787098e5e8da17abd8b1192c10fe600c
jahanzebkhancodes <jahanzebkhanwork@gmail.com>
2024-09-15 15:23:23 -0400

    Introducing Embedded Messages

--- main.py
+++ main.py
@@ -18,5 +18,12 @@ async def goodmorning(ctx):
 with open("token.txt") as file:
     token = file.read()
 
+@bot.command()
+async def ping(ctx):
+    ping_embed = discord.Embed(title="Ping", description="Latency in ms", color=discord.Color.blue())
+    ping_embed.add_field(name=f"{bot.user.name}'s Latency (ms): ", value=f"{round(bot.latency * 1000)}ms.", inline=False)
+    ping_embed.set_footer(text=f"Requested by {ctx.author.name}.", icon_url=ctx.author.avatar)
+    await ctx.send(embed=ping_embed)
+
 bot.run(token)
 
