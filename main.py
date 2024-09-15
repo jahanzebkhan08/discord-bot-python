b69b30778b8f49d32564e663ae5b4de9a2f04705
jahanzebkhancodes <jahanzebkhanwork@gmail.com>
2024-09-15 15:02:26 -0400

    Introducing Commands

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 async def on_ready():
     print("Bot ready!")
 
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f"Hello there, {ctx.author.mention}!")
+
+@bot.command(aliases=["gm", "morning"])
+async def goodmorning(ctx):
+    await ctx.send(f"Good morning, {ctx.author.mention}!")
+
 with open("token.txt") as file:
     token = file.read()
 
-bot.run("token")
+bot.run(token)
 
